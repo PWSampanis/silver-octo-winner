128462ca93612dfc5b5ba0848e9e8fc37fff611c
pwsampanis@gmail.com <pwsampanis@gmail.com>
2023-02-25 18:18:29 +0000

    added stg_fhv_tripdata and fact_fhv_trips

--- models/staging/stg_fhv_tripdata.sql
+++ models/staging/stg_fhv_tripdata.sql
@@ -1,3 +1,33 @@
 {{ config(materialized='view') }}
 
-select * from {{ source('staging','fhv_tripdata_raw')}}
+with tripdata as 
+(
+  select *,
+    row_number() over(partition by dispatching_base_num, pickup_datetime,dropOff_datetime,PUlocationID,DOlocationID,Affiliated_base_number ) as rn
+  from {{ source('staging','fhv_tripdata_raw') }}
+  where dispatching_base_num is not null 
+)
+select
+    -- identifiers
+   {{ dbt_utils.generate_surrogate_key(['dispatching_base_num','Affiliated_base_number', 'pickup_datetime','dropOff_datetime']) }} as tripid,
+    cast(dispatching_base_num as string) as dispatching_base_number,
+    cast(Affiliated_base_number as string) as affiliated_base_number,
+    cast(PUlocationID as integer) as  pickup_locationid,
+    cast(DOlocationID as integer) as dropoff_locationid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    SR_Flag
+from tripdata
+where rn = 1
+
+
+-- dbt build --m <model.sql> --var 'is_test_run: false'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
